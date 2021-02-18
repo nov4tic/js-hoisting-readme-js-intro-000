aa669f63f9c0d50e12ea24cbc757f79c3b3b6965
Dev Frias <ddfrias14@gmail.com>
2021-02-18 20:13:55 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -21,11 +21,12 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
+  var name = 'idk'
 
+  sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
-sayMy();
